fe9a997eadfa4d00b4411a9f3ac78309324955b3
unknown <nurdiansyah192@gmail.com>
2025-04-30 09:36:15 +0700

    Add print no header

--- __manifest__.py
+++ __manifest__.py
@@ -12,15 +12,19 @@
         'views/lww_receipt_template.xml',
         'views/bs_receipt_template.xml',
         # 'views/spartadua_receipt_template.xml',
+
         # DELIVERY ORDER
         'report/lww_do_report.xml',
         'views/lww_do_template.xml',
         'views/bs_do_template.xml',
         'views/spartadua_do_template.xml',
+
         # DELIVERY ORDER NO HEADER
         'views/lww_do_template_no_header.xml',
+
         # FORM VIEW
         'views/stock_picking_view.xml',
+        
         # SECURITY (ACCESS)
         # 'security/ir.model.access.csv',
     ],
