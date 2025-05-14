01291d4603da099fc74771c94895e3ed4e93bbe6
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-05-14 11:35:48 +0700

    Update sale_id fields

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -33,19 +33,28 @@ class StockPicking(models.Model):
             'value': {'project_id': False},
         }
     
-    @api.onchange('project_id')
-    def _onchange_project_id(self):
-        if self.project_id:
-            return {
-                'domain': {
-                    'sale_id': [('project_id', '=', self.project_id.id)]
-                },
-                'value': {'sale_id': False}
-            }
-        return {
-            'domain': {'sale_id': []},
-            'value': {'sale_id': False}
-        }
+    @api.depends('group_id', 'project_id.reinvoiced_sale_order_id')
+    def _compute_sale_id(self):
+        for picking in self:
+            if picking.group_id and picking.group_id.sale_id:
+                picking.sale_id = picking.group_id.sale_id
+            elif picking.project_id and picking.project_id.reinvoiced_sale_order_id:
+                picking.sale_id = picking.project_id.reinvoiced_sale_order_id
+            else:
+                picking.sale_id = False
+
+    def _set_sale_id(self):
+        for picking in self:
+            if picking.group_id:
+                picking.group_id.sale_id = picking.sale_id
+            else:
+                vals = {
+                    'sale_id': picking.sale_id.id,
+                    'name': picking.sale_id.name,
+                } if picking.sale_id else {}
+
+                pg = self.env['procurement.group'].create(vals)
+                picking.group_id = pg
 
     # print DO with header
     def action_print_report(self):
